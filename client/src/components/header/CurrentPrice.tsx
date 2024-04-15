51237941b247f939adced9e2892db94f9f8440a2
teoMarinov <teodor.b.marinov@gmail.com>
2024-04-15 13:52:42 +0300

    add navitaoin to NavbarIcons

--- client/src/components/header/CurrentPrice.tsx
+++ client/src/components/header/CurrentPrice.tsx
@@ -1,9 +1,12 @@
 import { EuroIcon } from "lucide-react";
+import { ChevronDown } from "lucide-react";
+
 const CurrentPrice = () => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center cursor-pointer">
       <p className="text-2xl pb-0.5">123.45</p>
-      <EuroIcon className="size-6" />
+      <EuroIcon className="size-6 ml-1" />
+      <ChevronDown className="size-5 ml-1" />
     </div>
   );
 };
--- client/src/components/header/NavIcons.tsx
+++ client/src/components/header/NavIcons.tsx
@@ -2,17 +2,31 @@ import { PersonIcon } from "@radix-ui/react-icons";
 import { HeartIcon } from "lucide-react";
 import { ShoppingBasket } from "lucide-react";
 import CurrentPrice from "./CurrentPrice";
+import { useNavigate } from "react-router-dom";
 
 const NavIcons = () => {
-    return (
-        <div className="flex items-center justify-center gap-x-3">
-                <PersonIcon className="h-6 w-6" />
-                <HeartIcon className="h-6 w-6" />
-                <ShoppingBasket className="h-6 w-6" />
-                <div className="border-l h-full ml-2 mr-1 border-black"/>
-                <CurrentPrice />
-        </div>
-    );
+  const nav = useNavigate();
+
+  return (
+    <div className="flex items-center justify-center gap-x-3 ">
+      <PersonIcon
+        onClick={() => nav("/register")}
+        className="h-6 w-6 cursor-pointer hover:scale-[110%]"
+      />
+      <HeartIcon
+        onClick={() => nav("/favorites")}
+        className="h-6 w-6 hover:scale-[110%] cursor-pointer"
+      />
+      <ShoppingBasket
+        onClick={() => nav("/cart")}
+        className="h-6 w-6 hover:scale-[120%] cursor-pointer"
+      />
+      <div className="border-l h-full ml-2 mr-1 border-black" />
+      <div onClick={() => nav("/cart")}>
+        <CurrentPrice />
+      </div>
+    </div>
+  );
 };
 
 export default NavIcons;
