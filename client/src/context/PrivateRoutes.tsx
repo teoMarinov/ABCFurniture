df223a88eb94ee89189c33a224cee93c2d437c98
teoMarinov <teodor.b.marinov@gmail.com>
2024-04-15 18:27:22 +0300

    change PrivateRoutes to look for user isntead of token

--- client/src/context/PrivateRoutes.tsx
+++ client/src/context/PrivateRoutes.tsx
@@ -1,10 +1,9 @@
-
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
 
 const PrivateRoutes = () => {
-  const user = useAuth();
-  if (!user.token) return <Navigate to="/login" />;
+  const { user } = useAuth();
+  if (!user) return <Navigate to="/login" />;
   return <Outlet />;
 };
 
