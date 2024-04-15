cbd5ce605b8cab568395fc37daf3c72b1c8af2fd
teoMarinov <teodor.b.marinov@gmail.com>
2024-04-15 18:29:04 +0300

    add navigate to home on header logo

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -2,13 +2,16 @@ import logo from "@/assets/logo.svg";
 
 import NavIcons from "./NavIcons";
 import SearchBar from "./SearchBar";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const nav = useNavigate();
+
   return (
     <div className="w-screen h-[8vh] shadow-sm border-b border-gray-400 shadow-gray-200 p-4 flex justify-between items-center px-32">
-      <img src={logo} />
+      <img src={logo} className="cursor-pointer" onClick={() => nav("/")} />
       <SearchBar />
-      <NavIcons /> 
+      <NavIcons nav={nav} />
     </div>
   );
 };
