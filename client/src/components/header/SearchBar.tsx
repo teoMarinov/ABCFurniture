28fe355bb90b7106ae431c92d059bafe328efb05
teoMarinov <teodor.b.marinov@gmail.com>
2024-04-18 17:51:05 +0300

    change input component ring from 2 to 1 globaly

--- client/src/components/header/SearchBar.tsx
+++ client/src/components/header/SearchBar.tsx
@@ -4,7 +4,7 @@ import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 const SearchBar = () => {
   return (
     <div className="relative w-[500px]">
-      <Input className="w-full rounded-full focus-visible:ring-1 focus-visible:ring-offset-0" />
+      <Input className="w-full rounded-full" />
       <div className="h-full w-12 rounded-r-full flex items-center justify-center absolute top-0 right-0 transition hover:bg-neutral-200">
         <MagnifyingGlassIcon className="h-6 w-6 z-50" />
       </div>
