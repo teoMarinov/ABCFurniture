aaea8778dfd7784322872608e8dc6e637143b421
teoMarinov <teodor.b.marinov@gmail.com>
2024-04-19 23:10:55 +0300

    change token expiration time to 7 days

--- server/src/main/java/com/abcfurniture/server/service/TokenService.java
+++ server/src/main/java/com/abcfurniture/server/service/TokenService.java
@@ -29,7 +29,8 @@ public class TokenService {
     private SecretKey Key;
 
     @Autowired
-    private static final long EXPIRATION_TIME = 86400000;
+    private static final long EXPIRATION_TIME = 86400000 * 7;
+//    1000 * 60 * 60 * 24
     public TokenService(){
         String secreteString = "843567893696976453275974432697R634976R738467TR678T34865R6834R8763T478378637664538745673865783678548735687R3";
         byte[] keyBytes = Base64.getDecoder().decode(secreteString.getBytes(StandardCharsets.UTF_8));
