a349aadecb98c709bdbb26161bf4e47624299113
teoMarinov <teodor.b.marinov@gmail.com>
2024-04-21 19:05:58 +0300

    add hover effect to categories

--- client/src/components/navbar/ColContainer.tsx
+++ client/src/components/navbar/ColContainer.tsx
@@ -6,12 +6,12 @@ interface ColContainerProps {
   options: string[];
 }
 const ColContainer = ({ title, options }: ColContainerProps) => {
-  const titleNormalized = title.replace(/\s+/g, "-").toLocaleLowerCase()
+  const titleNormalized = title.replace(/\s+/g, "-").toLocaleLowerCase();
   return (
     <div className="w-56 p-4">
-      <div className="flex items-center relative">
-        <ChevronRight className="size-4 absolute -left-4 top-[5px]" />
-        <h1 className="font-semibold underline cursor-pointer">
+      <div className="flex items-center relative group">
+        <ChevronRight className="size-4 absolute -left-4 top-[5px] group-hover:scale-125" />
+        <h1 className="font-semibold underline cursor-pointer group-hover:scale-105">
           <Link
             to={`products/${titleNormalized}`}
             className="w-fit cursor-pointer"
@@ -21,7 +21,10 @@ const ColContainer = ({ title, options }: ColContainerProps) => {
         </h1>
       </div>
       {options.map((option) => (
-        <div className="lg:text-sm text-xs my-4" key={option}>
+        <div
+          className="lg:text-sm text-xs my-4 transition hover:scale-105 ease-out"
+          key={option}
+        >
           <Link
             to={`products/${titleNormalized}/${option.replace(/\s+/g, "-")}`}
             className="w-fit cursor-pointer"
