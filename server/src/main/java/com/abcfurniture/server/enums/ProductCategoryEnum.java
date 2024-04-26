f7ea4b099463abee7dd001a9dcc0622ec52eb8c8
teoMarinov <teodor.b.marinov@gmail.com>
2024-04-26 16:44:30 +0300

    add not yet used category and sub category enums, will be implemented later

---
+++ server/src/main/java/com/abcfurniture/server/enums/ProductCategoryEnum.java
@@ -0,0 +1,15 @@
+//package com.abcfurniture.server.enums;
+//
+//import com.fasterxml.jackson.annotation.JsonCreator;
+//import com.fasterxml.jackson.annotation.JsonValue;
+//
+//public enum ProductCategoryEnum {
+//    kitchen,
+//    bedroom,
+//    living_dining,
+//    lighting,
+//    children,
+//    hallway,
+//    outdoor;
+//
+//}
