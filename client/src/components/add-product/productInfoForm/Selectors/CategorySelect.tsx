6fd8ea996e7330bda871634d68b7cc8a4bb927a8
teoMarinov <teodor.b.marinov@gmail.com>
2024-04-28 15:41:49 +0300

    add category selector

--- client/src/components/add-product/productInfoForm/Selectors/CategorySelect.tsx
+++ client/src/components/add-product/productInfoForm/Selectors/CategorySelect.tsx
@@ -1,11 +1,23 @@
+import { SelectGroup, SelectItem } from "@/components/ui/select";
+import Selector from "./Selector";
+
 interface CategorySelectProps {
+  field: any;
 }
-const CategorySelect = ({}:CategorySelectProps) => {
-    return (
-        <div>
-                CategorySelect
-        </div>
-    );
+const CategorySelect = ({ field }: CategorySelectProps) => {
+  return (
+    <Selector field={field} placeholder="Select a category">
+      <SelectGroup>
+        <SelectItem value="kitchen">Kitchen</SelectItem>
+        <SelectItem value="bedroom">Bedroom</SelectItem>
+        <SelectItem value="living_dining">Living Dining</SelectItem>
+        <SelectItem value="lighting">Lighting</SelectItem>
+        <SelectItem value="children">Children</SelectItem>
+        <SelectItem value="hallway">Hallway</SelectItem>
+        <SelectItem value="outdoor">Outdoor</SelectItem>
+      </SelectGroup>
+    </Selector>
+  );
 };
 
 export default CategorySelect;
