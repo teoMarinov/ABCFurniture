0fd372f43dd489acb7d7b2c2063da319458a89ee
teoMarinov <teodor.b.marinov@gmail.com>
2024-04-28 16:29:56 +0300

    Children options

---
+++ client/src/components/add-product/Selectors/ChildrenOptions.tsx
@@ -0,0 +1,13 @@
+import { SelectGroup, SelectItem, SelectLabel } from "@/components/ui/select";
+
+const ChildrenOptions = () => {
+  return (
+    <SelectGroup>
+      <SelectLabel>Children</SelectLabel>
+      <SelectItem value="kids_room">Kids Room</SelectItem>
+
+    </SelectGroup>
+  );
+};
+
+export default ChildrenOptions;
