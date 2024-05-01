338f8b907a9b5fffcd714827fbf64454cb3f58ab
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-01 16:02:46 +0300

    add productType

--- client/src/common/types.tsx
+++ client/src/common/types.tsx
@@ -3,5 +3,17 @@ export interface userDataType {
   email: string;
   name: string;
   role: "CUSTOMER" | "EMPLOYEE" | "MANAGER";
-  created_at: string
+  created_at: string;
 }
+
+export type productType = {
+  id: number;
+  name: string;
+  description: string;
+  category: string;
+  sub_category: string;
+  added_at: string;
+  price: number;
+  quantity: number;
+  images: string[];
+};
