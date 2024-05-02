2be82a5f5bcfad5b27baa540dcce2280fc3be799
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-02 17:46:03 +0300

    add category descrption repository

---
+++ server/src/main/java/com/abcfurniture/server/repository/CategoryDescriptionRepository.java
@@ -0,0 +1,7 @@
+package com.abcfurniture.server.repository;
+
+import com.abcfurniture.server.model.CategoryDescription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryDescriptionRepository extends JpaRepository<CategoryDescription, String> {
+}
