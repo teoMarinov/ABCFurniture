d0036621c5e537c1a851d6a5eda3934acc10ce4e
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-02 17:42:28 +0300

    fix lighting options

--- client/src/components/add-product/Selectors/subcategoryOptions/LightingOptions.tsx
+++ client/src/components/add-product/Selectors/subcategoryOptions/LightingOptions.tsx
@@ -4,12 +4,12 @@ const LightingOptions = () => {
   return (
     <SelectGroup>
       <SelectLabel>Lighting</SelectLabel>
-      <SelectItem value="spot_lighting">Spot Lighting</SelectItem>
+      <SelectItem value="spot-lighting">Spot Lighting</SelectItem>
       <SelectItem value="plafonds">Plafonds</SelectItem>
-      <SelectItem value="chandeliers_and_pendants">Chandeliers And Pendants</SelectItem>
-      <SelectItem value="wall_bracket_lamps">Wall Bracket Lamps</SelectItem>
-      <SelectItem value="reading_and_bedside_lamps">Reading And Bedside Lamps</SelectItem>
-      <SelectItem value="floor_lamps">Floor Lamps</SelectItem>
+      <SelectItem value="chandeliers-and-pendants">Chandeliers And Pendants</SelectItem>
+      <SelectItem value="wall-bracket-lamps">Wall Bracket Lamps</SelectItem>
+      <SelectItem value="reading-and-bedside-lamps">Reading And Bedside Lamps</SelectItem>
+      <SelectItem value="floor-lamps">Floor Lamps</SelectItem>
     </SelectGroup>
   );
 };
