48a39331639eaefe1b6336993e0da4b3ba4b2dce
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-02 18:10:43 +0300

    add sub category to backend

---
+++ server/src/main/java/com/abcfurniture/server/model/SubCategoryDescription.java
@@ -0,0 +1,52 @@
+package com.abcfurniture.server.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import org.hibernate.validator.constraints.UniqueElements;
+
+@Entity
+public class SubCategoryDescription {
+
+    @Id
+    @UniqueElements
+    private String subcategory_name;
+    private String image;
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    public String getSubcategory_name() {
+        return subcategory_name;
+    }
+
+    public void setSubcategory_name(String subcategory_name) {
+        this.subcategory_name = subcategory_name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public SubCategoryDescription(String subcategory_name, String image, String description) {
+        this.subcategory_name = subcategory_name;
+        this.image = image;
+        this.description = description;
+
+
+    }
+}
---
+++ server/src/main/java/com/abcfurniture/server/repository/SubCategoryDesciptionRepository.java
@@ -0,0 +1,7 @@
+package com.abcfurniture.server.repository;
+
+import com.abcfurniture.server.model.SubCategoryDescription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubCategoryDesciptionRepository extends JpaRepository<SubCategoryDescription, String> {
+}
