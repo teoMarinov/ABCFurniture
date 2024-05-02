19bf892acdc915c148989ad885aabae54eecf3ed
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-02 17:42:21 +0300

    fix kitchen options

--- client/src/components/add-product/Selectors/subcategoryOptions/KitchenOptions.tsx
+++ client/src/components/add-product/Selectors/subcategoryOptions/KitchenOptions.tsx
@@ -3,11 +3,11 @@ const KitchenOptions = () => {
   return (
     <SelectGroup>
       <SelectLabel>Kitchen</SelectLabel>
-      <SelectItem value="classic_and_rustic_kitchens">Classic And Rustic Kitchens</SelectItem>
-      <SelectItem value="modern_kitchens">Modern Kitchens</SelectItem>
+      <SelectItem value="classic-and-rustic-kitchens">Classic And Rustic Kitchens</SelectItem>
+      <SelectItem value="modern-kitchens">Modern Kitchens</SelectItem>
       <SelectItem value="sinks">Sinks</SelectItem>
-      <SelectItem value="mixing_taps">Mixing Taps</SelectItem>
-      <SelectItem value="kitchen_worktops">Kitchen Worktops</SelectItem>
+      <SelectItem value="mixing-taps">Mixing Taps</SelectItem>
+      <SelectItem value="kitchen-worktops">Kitchen Worktops</SelectItem>
       <SelectItem value="accessories">Accessories</SelectItem>
     </SelectGroup>
   );
