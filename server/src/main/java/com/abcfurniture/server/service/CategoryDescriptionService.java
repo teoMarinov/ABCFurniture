24627231be626269cd5116f07830e805185d37be
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-02 21:33:08 +0300

    create getmapping methods to for category descriptions

--- server/src/main/java/com/abcfurniture/server/service/CategoryDescriptionService.java
+++ server/src/main/java/com/abcfurniture/server/service/CategoryDescriptionService.java
@@ -1,9 +1,14 @@
 package com.abcfurniture.server.service;
 
 
+import com.abcfurniture.server.model.CategoryDescription;
+import com.abcfurniture.server.model.SubCategoryDescription;
 import com.abcfurniture.server.repository.CategoryDescriptionRepository;
+import com.abcfurniture.server.repository.SubCategoryDesciptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryDescriptionService {
@@ -11,7 +16,14 @@ public class CategoryDescriptionService {
     @Autowired
     private CategoryDescriptionRepository categoryDescriptionRepository;
 
-    public void changeCategoryDescription(){
+    @Autowired
+    SubCategoryDesciptionRepository subCategoryDesciptionRepository;
 
+    public List<CategoryDescription> getMain(){
+        return categoryDescriptionRepository.findAll();
+    }
+
+    public List<SubCategoryDescription> getSub(){
+        return subCategoryDesciptionRepository.findAll();
     }
 }
