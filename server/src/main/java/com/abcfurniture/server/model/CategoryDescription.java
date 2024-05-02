92b803a120262097410396c8c57ade58024dcf2d
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-02 17:45:50 +0300

    add category descption model

---
+++ server/src/main/java/com/abcfurniture/server/model/CategoryDescription.java
@@ -0,0 +1,52 @@
+package com.abcfurniture.server.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import org.hibernate.validator.constraints.UniqueElements;
+
+@Entity
+public class CategoryDescription {
+
+    @Id
+    @UniqueElements
+    private String category_name;
+    private String image;
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    public String getCategory_name() {
+        return category_name;
+    }
+
+    public void setCategory_name(String category_name) {
+        this.category_name = category_name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public CategoryDescription(String category_name, String image, String description) {
+        this.category_name = category_name;
+        this.image = image;
+        this.description = description;
+
+
+    }
+}
