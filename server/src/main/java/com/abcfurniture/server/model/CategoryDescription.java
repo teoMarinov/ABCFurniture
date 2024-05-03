4c10f1facb952cc740a079b5b938714e1252f05d
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-03 19:21:27 +0300

    add OneToMany connection between category and sub_category

--- server/src/main/java/com/abcfurniture/server/model/CategoryDescription.java
+++ server/src/main/java/com/abcfurniture/server/model/CategoryDescription.java
@@ -4,6 +4,9 @@ package com.abcfurniture.server.model;
 import jakarta.persistence.*;
 import org.hibernate.validator.constraints.UniqueElements;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class CategoryDescription {
 
@@ -17,14 +20,28 @@ public class CategoryDescription {
     @Column(columnDefinition = "TEXT")
     private String description;
 
+    @OneToMany(cascade =  CascadeType.ALL)
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private List<SubCategoryDescription> subcategories = new ArrayList<SubCategoryDescription>();
+
     public CategoryDescription() {
     }
 
-    public CategoryDescription(String category_name, String image, String description) {
+    public CategoryDescription(String category_name, String image, String description, List<SubCategoryDescription> subcategories) {
         this.category_name = category_name;
         this.image = image;
         this.description = description;
+        this.subcategories = subcategories;
     }
+
+    public List<SubCategoryDescription> getSubcategories() {
+        return subcategories;
+    }
+
+    public void setSubcategories(List<SubCategoryDescription> subcategories) {
+        this.subcategories = subcategories;
+    }
+
     public String getCategory_name() {
         return category_name;
     }
