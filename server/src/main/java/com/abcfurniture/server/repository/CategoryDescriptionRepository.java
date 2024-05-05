5099a8e2d1298715581b24bb0e24566826053e9f
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-05 21:26:56 +0300

    fix naming error in CategoryDescriptionRepository

--- server/src/main/java/com/abcfurniture/server/repository/CategoryDescriptionRepository.java
+++ server/src/main/java/com/abcfurniture/server/repository/CategoryDescriptionRepository.java
@@ -3,5 +3,8 @@ package com.abcfurniture.server.repository;
 import com.abcfurniture.server.model.CategoryDescription;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryDescriptionRepository extends JpaRepository<CategoryDescription, Integer> {
+    Optional<CategoryDescription> findByCategoryName(String categoryName);
 }
