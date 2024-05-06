3cc7967212a6370f2b6a027e2dcc3280925bb2b6
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-06 20:42:13 +0300

    add findAllWithoutConnections to CategoryDescriptionRepository

--- server/src/main/java/com/abcfurniture/server/repository/CategoryDescriptionRepository.java
+++ server/src/main/java/com/abcfurniture/server/repository/CategoryDescriptionRepository.java
@@ -1,10 +1,16 @@
 package com.abcfurniture.server.repository;
 
+import com.abcfurniture.server.dto.CategoryDescriptionDTO;
 import com.abcfurniture.server.model.CategoryDescription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryDescriptionRepository extends JpaRepository<CategoryDescription, Integer> {
+
+    @Query("SELECT new com.abcfurniture.server.dto.CategoryDescriptionDTO(cd.id, cd.categoryName, cd.image, cd.description) FROM CategoryDescription cd")
+    List<CategoryDescriptionDTO> findAllWithoutConnections();
     Optional<CategoryDescription> findByCategoryName(String categoryName);
 }
