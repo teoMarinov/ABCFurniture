8c8722f4c18283875abb70d3dff4cf10a7a8b130
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-07 20:26:56 +0300

    crfeate CategoryContainer

---
+++ client/src/components/common/CategoryContainer.tsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { Camera } from "lucide-react";
+
+interface CategoryContainerProps {
+  name: string;
+  image: string;
+}
+const CategoryContainer = ({ name, image }: CategoryContainerProps) => {
+  const nav = useNavigate();
+  return (
+    <div
+      onClick={() => nav(`/products/${name}`)}
+      className="border-2 shadow-md rounded-md h-[450px] cursor-pointer "
+    >
+      <div className="w-full h-[85%]">
+        {image ? (
+          <img src={image} />
+        ) : (
+          <div className="size-full grid place-items-center bg-slate-200">
+            <Camera className="size-10" />
+          </div>
+        )}
+      </div>
+      <div className="grid place-items-center h-[15%] rounded-b-md text-2xl capitalize">
+        <p className="font-serif">{name?.replace(/-/g, " ")}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryContainer;
