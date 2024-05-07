583a11214ed4e3ab50c63e36d86d55a858b1af27
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-07 21:15:45 +0300

    implement getting category and its subcategory info

--- client/src/components/categoryDetails/SubCategories.tsx
+++ client/src/components/categoryDetails/SubCategories.tsx
@@ -1,14 +1,30 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { request } from "@/config/axios-helper";
+import CategoryDisplay from "./CategoryDisplay";
+import { CategoryInfoType } from "./MainCategories";
+
+export type SubCategoryInfo = {
+  subcategory_name: string;
+  description: string;
+  image: string;
+};
+
+interface FullCategoryInfoType extends CategoryInfoType {
+  subcategories: SubCategoryInfo[];
+}
 
 const SubCategories = () => {
   const params = useParams();
-  console.log("🚀 ~ CategoryDetails ~ params:", params.category);
 
-  return (
-    <div>
-      <p>{params.category}</p>
-    </div>
-  );
+  const [categoryIfno, setCategoryIfno] = useState<FullCategoryInfoType | null>(null);
+  console.log("🚀 ~ SubCategories ~ categoryIfno:", categoryIfno?.subcategories)
+  useEffect(() => {
+    request("get", `/category/${params.category}`).then(({ data }) => {
+      setCategoryIfno(data);
+    });
+  }, [params.category]);
+  return (<>{categoryIfno?.categoryName}</>)
 };
 
 export default SubCategories;
