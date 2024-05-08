ad610924e90d5f53393b0999e48d25bec373bfb1
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-09 01:01:10 +0300

    add reponsive design to CategoryInfoBox

--- client/src/components/categoryDetails/CategoryInfoBox.tsx
+++ client/src/components/categoryDetails/CategoryInfoBox.tsx
@@ -11,17 +11,17 @@ const CategoryInfoBox = ({
   image,
 }: CategoryInfoBoxProps) => {
   return (
-    <div className="flex w-[800px] border-2 border-gray-300 mt-5 mb-8 relative">
+    <div className="flex xl:w-[800px] w-full border-2 border-gray-300 mt-5 mb-8 relative rounded-md">
       <div className="p-5">
         <div className=" h-[150px] min-w-[150px] bg-slate-200 grid place-items-center">
           {image ? <img src={image} className="size-[150px]" /> : <Camera />}
         </div>
       </div>
-      <div className="w-full">
-        <h1 className="pt-2 pb-3 text-2xl capitalize w-full text-center">
+      <div className="w-full h-[165px] overflow-hidden text-justify pr-5">
+        <h1 className="pt-2 pb-3 text-2xl capitalize absolute left-[47%]">
           {name}
         </h1>
-        <p>{description}</p>
+        <p className="mt-12 sm:text-base text-sm ">{description}</p>
       </div>
 
       <div className="relative top-1 right-8">
