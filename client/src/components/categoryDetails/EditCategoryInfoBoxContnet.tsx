7d697d0f291b309ce38d3c39ceca6820acfc63cf
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-09 00:52:04 +0300

    create EditCategoryIngoBoxContent layout

--- client/src/components/categoryDetails/EditCategoryInfoBoxContnet.tsx
+++ client/src/components/categoryDetails/EditCategoryInfoBoxContnet.tsx
@@ -1,40 +1,122 @@
 import {
   AlertDialog,
-  AlertDialogAction,
   AlertDialogCancel,
   AlertDialogContent,
-  AlertDialogDescription,
   AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import { EditCategoryInfo } from "@/schemas/category-schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+
+import * as z from "zod";
+import { request } from "@/utils/axios-helper";
+import uploadImage from "@/utils/upload-image-cloudinary";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+import SingleImageHandler from "../common/SingleImageHandler";
+import { PenBox } from "lucide-react";
 
 interface EditCategoryInfoBoxContnetProps {
-  name?: string;
   description?: string;
   image?: string;
 }
 const EditCategoryInfoBoxContnet = ({
-  name,
-  description,
-  image,
+  description = "",
+  image = "",
 }: EditCategoryInfoBoxContnetProps) => {
+  const [newDescription, setNewDescription] = useState("");
+  const [newImage, setNewImage] = useState("");
+
+  const [isPending, setIsPending] = useState(false);
+
+  const form = useForm<z.infer<typeof EditCategoryInfo>>({
+    resolver: zodResolver(EditCategoryInfo),
+    defaultValues: {
+      description,
+      image,
+    },
+  });
+
+  const onCancel = () => {
+    form.reset();
+    setNewImage("");
+  };
+
+  const onSubmit = async (values: z.infer<typeof EditCategoryInfo>) => {
+    console.log(values);
+    setTimeout(() => {
+      // window.location.reload();
+    }, 500);
+    // const imageUrls: string[] = [];
+
+    // setIsPending(true);
+    // const result = await uploadImage(values.image);
+    // imageUrls.push(result);
+
+    // request("post", "/product/new", { ...values, images: imageUrls })
+    //   .then(() => {
+    //     setIsPending(false);
+    //   })
+    //   .catch((err) => console.error(err));
+  };
+
   return (
     <AlertDialog>
-      <AlertDialogTrigger>Open</AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
-        </AlertDialogFooter>
+      <AlertDialogTrigger asChild className="absolute">
+        <PenBox className="hover:scale-110 transition cursor-pointer size-7 p-0.5" />
+      </AlertDialogTrigger>
+      <AlertDialogContent className="max-w-fit gap-6">
+        <Form {...form}>
+          <form className="w-[70vw] flex flex-row-reverse justify-between">
+            <div className="w-2/3">
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Textarea
+                        {...field}
+                        disabled={isPending}
+                        placeholder="Category Description"
+                        className="h-96 resize-none"
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="size-96">
+              <SingleImageHandler
+                addToForm={(img) => form.setValue("image", img)}
+                imagePreview={newImage}
+                setImagePreview={setNewImage}
+                removeFromForm={() => form.resetField("image")}
+              />
+            </div>
+          </form>
+          <AlertDialogFooter>
+            <AlertDialogCancel
+              className="w-1/2"
+              disabled={isPending}
+              onClick={form.handleSubmit(onSubmit)}
+            >
+              Save
+            </AlertDialogCancel>
+            <AlertDialogCancel className="w-1/2" onClick={onCancel}>
+              Cancel
+            </AlertDialogCancel>
+          </AlertDialogFooter>
+        </Form>
       </AlertDialogContent>
     </AlertDialog>
   );
