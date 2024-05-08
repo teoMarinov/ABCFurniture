2205cda1d96274586a38aa00da9a842d565c9b40
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-08 20:30:00 +0300

    changer MultiImageHandler css to fit into its parent div size

--- client/src/components/common/MultiImageHandler.tsx
+++ client/src/components/common/MultiImageHandler.tsx
@@ -55,45 +55,43 @@ const MultiImageHandler = ({
   }, [imagePreview]);
 
   return (
-    <div className="h-[790px] xl:w-1/3 flex items-center">
-      <div className="h-full w-full flex flex-col gap-4 ">
-        <div className="w-full h-3/4 flex  items-center justify-center border-2 shadow-md rounded-md p-2 group relative">
-          <img className="max-h-full max-w-full" src={imagePreview[display!]} />
-          <PlusCircle
-            size={100}
-            className="group-hover:scale-125 transition absolute -z-10"
-          />
-          <input
-            className="cursor-pointer file:cursor-pointer xl:w-[590px] w-full h-[580px] opacity-0 z-0 absolute"
-            type="file"
-            onChange={handleOnChange}
-            multiple
-          />
-        </div>
-        {imagePreview[0] && (
-          <Carousel>
-            <CarouselContent className="ml-[1px]">
-              {imagePreview.map((image: string, index) => (
-                <CarouselItem
-                  key={index}
-                  className="group h-[180px] basis-1/3 flex items-center justify-center border-2 shadow-md rounded-md p-1 relative cursor-pointer overflow-hidden"
-                  onClick={() => setDisplay(index)}
-                >
-                  <Trash2
-                    className="z-50 size-7 absolute top-0 right-0 translate-x-6 group-hover:translate-x-0 transition text-red-700 hover:scale-125 cursor-pointer rounded-full p-1"
-                    onClick={() => removeImage(index)}
-                  />
-                  <img className="max-h-full max-w-full -z-50" src={image} />
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <div className="xl:block hidden">
-              <CarouselPrevious />
-              <CarouselNext />
-            </div>
-          </Carousel>
-        )}
+    <div className="size-full flex flex-col gap-4">
+      <div className="w-full h-3/4 flex items-center justify-center border-2 shadow-md rounded-md p-2 group relative">
+        <img className="max-h-full max-w-full" src={imagePreview[display!]} />
+        <PlusCircle
+          size={100}
+          className="group-hover:scale-125 transition absolute -z-10"
+        />
+        <input
+          className="cursor-pointer file:cursor-pointer w-full h-full opacity-0 z-0 absolute"
+          type="file"
+          onChange={handleOnChange}
+          multiple
+        />
       </div>
+      {imagePreview[0] && (
+        <Carousel>
+          <CarouselContent className="ml-[1px] gap-[0.40%]">
+            {imagePreview.map((image: string, index) => (
+              <CarouselItem
+                key={index}
+                className="group h-[180px] basis-[33%] flex items-center justify-center border-2 shadow-md rounded-md p-1 relative cursor-pointer overflow-hidden"
+                onClick={() => setDisplay(index)}
+              >
+                <Trash2
+                  className="z-50 size-7 absolute top-0 right-0 translate-x-6 group-hover:translate-x-0 transition text-red-700 hover:scale-125 cursor-pointer rounded-full p-1"
+                  onClick={() => removeImage(index)}
+                />
+                <img className="max-h-full max-w-full -z-50" src={image} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <div className="xl:block hidden">
+            <CarouselPrevious />
+            <CarouselNext />
+          </div>
+        </Carousel>
+      )}
     </div>
   );
 };
