87b86d5c426bcdbd53efc08d99df508ba76e54e7
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-08 17:32:54 +0300

    delete wrong import

--- client/src/components/categoryDetails/MainCategories.tsx
+++ client/src/components/categoryDetails/MainCategories.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { request } from "@/config/axios-helper";
-import CategoryDisplay from "./CategoryDisplay";
 import CategoryContainer from "./CategoryContainer";
 
 export interface CategoryInfoType {
