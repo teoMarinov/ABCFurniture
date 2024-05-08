348a0fbd59a9ab98435d85e3a0e1cf3715be84e1
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-08 17:32:42 +0300

    Add content to SubCategories page

--- client/src/components/categoryDetails/SubCategories.tsx
+++ client/src/components/categoryDetails/SubCategories.tsx
@@ -1,8 +1,9 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { request } from "@/config/axios-helper";
-import CategoryDisplay from "./CategoryDisplay";
 import { CategoryInfoType } from "./MainCategories";
+import CategoryContainer from "./CategoryContainer";
+import CategoryInfoBox from "./CategoryInfoBox";
 
 export type SubCategoryInfo = {
   subcategory_name: string;
@@ -17,14 +18,33 @@ interface FullCategoryInfoType extends CategoryInfoType {
 const SubCategories = () => {
   const params = useParams();
 
-  const [categoryIfno, setCategoryIfno] = useState<FullCategoryInfoType | null>(null);
-  console.log("🚀 ~ SubCategories ~ categoryIfno:", categoryIfno?.subcategories)
+  const [categoryIfno, setCategoryIfno] = useState<FullCategoryInfoType | null>(
+    null
+  );
+
   useEffect(() => {
     request("get", `/category/${params.category}`).then(({ data }) => {
       setCategoryIfno(data);
     });
   }, [params.category]);
-  return (<>{categoryIfno?.categoryName}</>)
+  return (
+    <div className="w-full flex flex-col items-center">
+      <CategoryInfoBox
+        name={categoryIfno?.categoryName}
+        description={categoryIfno?.description}
+        image={categoryIfno?.image}
+      />
+      <div className="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-4 2xl:w-[90%] w-full 2xl:p-0 px-4">
+        {categoryIfno?.subcategories.map((info) => (
+          <CategoryContainer
+            key={info.subcategory_name}
+            name={info.subcategory_name}
+            image={info.image}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SubCategories;
