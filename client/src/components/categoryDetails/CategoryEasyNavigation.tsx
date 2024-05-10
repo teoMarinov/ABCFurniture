a5da633b37a43c1adbc2bb6202d4b5a30f1f08be
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-10 23:57:52 +0300

    add styling

--- client/src/components/categoryDetails/CategoryEasyNavigation.tsx
+++ client/src/components/categoryDetails/CategoryEasyNavigation.tsx
@@ -1,33 +1,55 @@
 import { Link } from "react-router-dom";
 import clsx from "clsx";
 interface CategoryEasyNavigationProps {
-  currentlyOpen: string;
+  currentlyOpen?: string;
 }
 const CategoryEasyNavigation = ({
   currentlyOpen,
 }: CategoryEasyNavigationProps) => {
   return (
-    <div className="lg:block hidden h-full border-2 p-2 px-4 text-xl">
+    <div className="lg:flex flex-col hidden h-full border-2 border-gray-700 p-5 space-y-6 text-2xl rounded-md">
       <Link to="/products/kitchen">
-        <p className={clsx(currentlyOpen === "kitchen" && "font-bold")}>Kitchen</p>
+        <p className={clsx(currentlyOpen === "kitchen" && "underline")}>
+          Kitchen
+        </p>
       </Link>
       <Link to="/products/bedroom">
-        <p className={clsx(currentlyOpen === "bedroom" && "font-bold")}>Bedroom</p>
+        <p className={clsx(currentlyOpen === "bedroom" && "underline")}>
+          Bedroom
+        </p>
       </Link>
       <Link to="/products/living-and-dining-room">
-        <p className={clsx(currentlyOpen === "living-and-dining-room" && "font-bold")}>Living And Dining Room</p>
+        <p
+          className={clsx(
+            currentlyOpen === "living-and-dining-room" && "underline"
+          )}
+        >
+          Living And Dining Room
+        </p>
       </Link>
       <Link to="/products/lighting">
-        <p className={clsx(currentlyOpen === "lighting" && "font-bold")}>Lighting</p>
+        <p className={clsx(currentlyOpen === "lighting" && "underline")}>
+          Lighting
+        </p>
       </Link>
       <Link to="/products/children-furniture">
-        <p className={clsx(currentlyOpen === "children-furniture" && "font-bold")}>Children Furniture</p>
+        <p
+          className={clsx(
+            currentlyOpen === "children-furniture" && "underline"
+          )}
+        >
+          Children Furniture
+        </p>
       </Link>
       <Link to="/products/hallway">
-        <p className={clsx(currentlyOpen === "hallway" && "font-bold")}>Hallway</p>
+        <p className={clsx(currentlyOpen === "hallway" && "underline")}>
+          Hallway
+        </p>
       </Link>
       <Link to="/products/outdoor-garden">
-        <p className={clsx(currentlyOpen === "outdoor-garden" && "font-bold")}>Outdoor Garden</p>
+        <p className={clsx(currentlyOpen === "outdoor-garden" && "underline")}>
+          Outdoor Garden
+        </p>
       </Link>
     </div>
   );
