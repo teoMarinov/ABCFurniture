53d357de58788b4bd042c7d7ddfb6cd053be8367
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-13 17:19:17 +0300

    add initial ProductDisplay elements

--- client/src/components/products/ProductDisplay.tsx
+++ client/src/components/products/ProductDisplay.tsx
@@ -1,9 +1,44 @@
+import { useParams } from "react-router-dom";
+import CategoryInfoBox from "../categoryDetails/CategoryInfoBox";
+import { useState, useEffect } from "react";
+import { productType } from "@/common/types";
+import { request } from "@/utils/axios-helper";
+import { SubCategoryInfo } from "../categoryDetails/SubCategoriesList";
+import CategoryEasyNavigation from "../categoryDetails/CategoryEasyNavigation";
 const ProductDisplay = () => {
-    return (
-        <div>
-                Your products will be here
-        </div>
-    );
+  const params = useParams();
+  const category = params.category;
+  const subCategory = params.subcategory;
+
+  const [products, setProducts] = useState<productType[]>([]);
+  const [categoryIfno, setCategoryIfno] = useState<SubCategoryInfo | null>(
+    null
+  );
+
+  useEffect(() => {
+    // request("get", `/category/${category}`).then(({ data }) => {
+    //   // setCategoryIfno(data);
+    //   console.log(data);
+    // });
+    request("get", `/category/sub/${subCategory}`).then(({ data }) => {
+      setCategoryIfno(data);
+    });
+    request("get", `/product/${subCategory}`).then(({ data }) => {
+      setProducts(data);
+    });
+  }, [category, subCategory]);
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full">
+      <CategoryInfoBox
+        name={categoryIfno?.subcategoryName}
+        description={categoryIfno?.description}
+        image={categoryIfno?.image}
+      />
+      <CategoryEasyNavigation />
+      Your products will be here
+    </div>
+  );
 };
 
 export default ProductDisplay;
