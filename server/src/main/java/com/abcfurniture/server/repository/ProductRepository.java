db690b5b88f650ef1198875de38b503468150832
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-13 11:18:12 +0300

    add get all products by sub category repository

--- server/src/main/java/com/abcfurniture/server/repository/ProductRepository.java
+++ server/src/main/java/com/abcfurniture/server/repository/ProductRepository.java
@@ -3,8 +3,9 @@ package com.abcfurniture.server.repository;
 import com.abcfurniture.server.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-
+    Optional<List<Product>> findAllBySubCategory(String sub_category_name);
 }
