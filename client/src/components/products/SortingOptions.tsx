36ce05b09f7abbe6dc1037864d844daf9cfb9c86
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-15 15:49:32 +0300

    add SortingOptions basic layout

--- client/src/components/products/SortingOptions.tsx
+++ client/src/components/products/SortingOptions.tsx
@@ -1,9 +1,20 @@
 const SortingOptions = () => {
-    return (
-        <div className="h-20">
-                SortingOptions
+  return (
+    <div className="py-4 px-2">
+      <div className="flex items-center justify-between w-full">
+        <div className="flex gap-x-3">
+          <div className="size-9 border border-black rounded-md">1</div>
+          <div className="size-9 border border-black rounded-md">2</div>
         </div>
-    );
+        <div className="flex gap-x-1 items-center">
+          <p>Sort by:</p>
+          <div className="w-40 border border-black p-1 rounded-md mr-24">name ^</div>
+          <p>Show: </p>{" "}
+          <div className="w-40 border border-black p-1 rounded-md">12</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SortingOptions;
