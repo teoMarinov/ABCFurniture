d70b7de04c412755793b1a6e817d115030acfb33
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-15 15:49:17 +0300

    change easy navigation style

--- client/src/components/products/SubCategoryEasyNavigation.tsx
+++ client/src/components/products/SubCategoryEasyNavigation.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 interface SubCategoryEasyNavigationProps {
   category: string;
-  options: string[];
+  options: { subcategoryName: string }[];
   currentlyOpen?: string;
 }
 const SubCategoryEasyNavigation = ({
@@ -12,27 +12,32 @@ const SubCategoryEasyNavigation = ({
   currentlyOpen,
 }: SubCategoryEasyNavigationProps) => {
   return (
-    <div className="lg:flex flex-col hidden h-full border-2 capitalize border-gray-700 w-80 items-center text-2xl rounded-md">
-      <div className="w-full bg-neutral-700 py-4">
-        <h1 className="text-white w-full text-center text-3xl hover:-translate-y-0.5">
+    <div className="xl:flex flex-col hidden h-full border-xs capitalize border-gray-400 items-center text-xl">
+      <div className="w-full bg-black py-4">
+        <h1 className="text-white w-full text-center text-2xl hover:scale-105">
           <Link to={`/${category}`}>{category?.replace(/-/g, " ")}</Link>
         </h1>
       </div>
-      <div className="flex flex-col space-y-6 py-5">
+      <div className="flex flex-col w-80">
         {options.map((option) => (
-          <Link
-            to={`/${category}/${option.subcategoryName}`}
+          <div
             key={option.subcategoryName}
+            className="border-b border-x border-black border-x-gray-400 w-full flex justify-center"
           >
-            <p
-              className={clsx(
-                "hover:-translate-y-0.5",
-                currentlyOpen === option.subcategoryName && "underline"
-              )}
-            >
-              {option.subcategoryName?.replace(/-/g, " ")}
-            </p>
-          </Link>
+            <div className="py-4 w-[267px]">
+              <Link to={`/${category}/${option.subcategoryName}`}>
+                <p
+                  className={clsx(
+                    "hover:text-emerald-500",
+                    currentlyOpen === option.subcategoryName &&
+                      "text-emerald-500 font-black"
+                  )}
+                >
+                  {option.subcategoryName?.replace(/-/g, " ")}
+                </p>
+              </Link>
+            </div>
+          </div>
         ))}
       </div>
     </div>
