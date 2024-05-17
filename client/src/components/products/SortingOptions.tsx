4b47331ee46661c935bdc992688da2034093a469
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-17 23:55:56 +0300

    change SortingOptions with ToggleGroup and ptionSelect

--- client/src/components/products/SortingOptions.tsx
+++ client/src/components/products/SortingOptions.tsx
@@ -1,25 +1,36 @@
 import { useState } from "react";
 import { Slider } from "../ui/slider";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { ListBulletIcon, ViewGridIcon } from "@radix-ui/react-icons";
+import OptionSelect from "./OptionSelect";
+import SortByOptions from "./SortByOptions";
+interface SortingOptionsProps {
+  handleChange: (val: string) => void;
 
-const SortingOptions = () => {
+}
+const SortingOptions = ({handleChange}: SortingOptionsProps) => {
   const min = 0;
   const max = 999;
   const [range, setRange] = useState([min, max]);
 
-  const setHighestPrice = () => {
-    setRange([range[0], 780]);
-  };
-  const setLowestPrice = () => {
-    setRange([100, range[1]]);
-  };
+  // const setHighestPrice = () => {
+  //   setRange([range[0], 780]);
+  // };
+  // const setLowestPrice = () => {
+  //   setRange([100, range[1]]);
+  // };
   return (
     <div className="py-4 ">
       <div className="flex items-center justify-between w-full">
         <div className="flex gap-x-3">
-          <div className="size-9 border border-black rounded-md">1</div>
-          <div className="size-9 border border-black rounded-md">
-            2
-          </div>
+          <ToggleGroup type="single" defaultValue="List">
+            <ToggleGroupItem value="List">
+              <ListBulletIcon />
+            </ToggleGroupItem>
+            <ToggleGroupItem value="Grid" className="  :bg-red-500">
+              <ViewGridIcon />
+            </ToggleGroupItem>
+          </ToggleGroup>
         </div>
         a:{range[0]} <br />
         b:{range[1]}
@@ -31,10 +42,9 @@ const SortingOptions = () => {
           onValueChange={setRange}
         />
         <div className="flex gap-x-1 items-center">
-          <p>Sort by:</p>
-          <div className="w-40 border border-black p-1 rounded-md mr-24">
-            name ^
-          </div>
+          <OptionSelect title="Sort by" handleChange={handleChange} >
+            <SortByOptions />
+          </OptionSelect>
           <p>Show: </p>{" "}
           <div className="w-40 border border-black p-1 rounded-md">12</div>
         </div>
