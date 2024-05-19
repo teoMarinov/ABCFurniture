132349923853fcc87434748a748f81675f353385
teoMarinov <teodor.b.marinov@gmail.com>
2024-05-19 20:09:31 +0300

    finish PriceRange style

--- client/src/components/products/PriceRangeSlider.tsx
+++ client/src/components/products/PriceRangeSlider.tsx
@@ -1,4 +1,3 @@
-import { ChangeEvent, ChangeEventHandler } from "react";
 import { Input } from "../ui/input";
 import { Slider } from "../ui/slider";
 interface PriceRangeSliderProps {
@@ -19,23 +18,23 @@ const PriceRangeSlider = ({
   const handleChangeHighest = (e: React.ChangeEvent<HTMLInputElement>) => {
     setRange([range[1], Number(e.target.value)]);
   };
+  const enterPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      console.log(range);
+    }
+  }
   return (
-    <div className="bg-black py-2 w-full mt-3.5 text-white">
-      <Slider
-        min={min}
-        max={max}
-        step={1}
-        value={range}
-        onValueChange={setRange}
-        className="w-80"
-      />
-      <div className="flex justify-between px-5">
+    <div className="bg-black xl:flex hidden py-2 w-full mt-3.5 text-white flex-col items-center">
+      <div
+        onKeyDown={enterPress}
+        className="flex w-full justify-between px-4 my-2"
+      >
         <div className="flex items-center">
           <p>Min:</p>
           <Input
             value={range[0]!}
             onChange={handleChangeLowest}
-            className="h-s5 w-12 text-black"
+            className="h-s5 w-20 text-black ml-2 "
           />
         </div>
         <div className="flex items-center">
@@ -43,10 +42,18 @@ const PriceRangeSlider = ({
           <Input
             value={range[1]!}
             onChange={handleChangeHighest}
-            className="h-s5 w-12 text-black"
+            className="h-s5 w-20 text-black ml-2"
           />
         </div>
       </div>
+      <Slider
+        min={min}
+        max={max}
+        step={1}
+        value={range}
+        onValueChange={setRange}
+        className="mb-2 w-72 mt-4"
+      />
     </div>
   );
 };
